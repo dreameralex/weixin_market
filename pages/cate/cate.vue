3fba100ad7bdf2ba17d1e8aad09485998acd9793
Alex <zhangyh8216@sina.com>
2024-03-16 21:24:26 +0800

    动态渲染左侧的一级分类列表

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -3,38 +3,9 @@
 		<view class="scroll-view-container">
 			<!-- 左侧的滑动区-->
 			<scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
-				<view class="left-scroll-view-item active">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
-				<view class="left-scroll-view-item">xxx</view>
+				<block v-for="(item, i) in cateList" :key = "i">
+					<view :class="['left-scroll-view-item', i === active ? 'active':'']" @click="activeChanged(i)">{{item.cat_name}}</view>
+				</block>
 			</scroll-view>
 			<!-- 右侧的滑动区-->
 			<scroll-view class="right-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
@@ -80,7 +51,9 @@
 		data() {
 			return {
 				//当前设备可用的高度
-				wh: 0
+				wh: 0,
+				cateList: [],
+				active: 0
 			};
 		},
 		
@@ -89,6 +62,24 @@
 			const sysInfo = uni.getSystemInfoSync()
 			// 为 wh 窗口可用高度动态赋值
 			this.wh = sysInfo.windowHeight
+			//获取分类列表的数据
+			this.getCateList()
+		},
+		methods: {
+			//获取分类列表的数据
+			async getCateList(){
+				// 发起请求
+				const { data: res } = await uni.$http.get('/api/public/v1/categories')
+				// 判断是否获取失败
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 转存数据
+				this.cateList = res.message
+			},
+			
+			//点击的设定为active
+			activeChanged(i){
+				this.active = i
+			}
 		}
 	}
 	
