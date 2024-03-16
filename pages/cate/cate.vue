5896a0fbd8541bdb1139d2bf3e1eecead09a73c8
Alex <zhangyh8216@sina.com>
2024-03-16 20:57:27 +0800

    美化页面结构

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -3,38 +3,38 @@
 		<view class="scroll-view-container">
 			<!-- 左侧的滑动区-->
 			<scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
-				<view>xxx</view>
+				<view class="left-scroll-view-item active">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
+				<view class="left-scroll-view-item">xxx</view>
 			</scroll-view>
 			<!-- 右侧的滑动区-->
 			<scroll-view class="right-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
@@ -102,5 +102,27 @@
 .left-scroll-view{
 	width: 120 px;
 }
-
+.left-scroll-view-item{
+	background-color: #f7f7f7;
+	line-height: 60px;
+	text-align: center;
+	font-size: 12px;
+	
+	&.active{
+		background-color: #ffffff;
+		position: relative;
+		
+		&::before {
+			content: ' ';
+			display: block;
+			width: 3px;
+			height: 30px;
+			background-color: coral;
+			position: absolute;
+			top: 50%;
+			left: 0;
+			transform: translateY(-50%);
+		}
+	}
+}
 </style>
