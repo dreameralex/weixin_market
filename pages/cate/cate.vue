2e93888d0a8e884b2e5ce8f0cbf1dc87c5541430
Alex <zhangyh8216@sina.com>
2024-03-16 20:45:15 +0800

    定义页面结构, 动态计算窗口的剩余高度

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -2,7 +2,23 @@
 	<view>
 		<view class="scroll-view-container">
 			<!-- 左侧的滑动区-->
-			<scroll-view scroll-y="true" style="height: 300px">
+			<scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
 				<view>xxx</view>
 				<view>xxx</view>
 				<view>xxx</view>
@@ -21,7 +37,23 @@
 				<view>xxx</view>
 			</scroll-view>
 			<!-- 右侧的滑动区-->
-			<scroll-view scroll-y="true" style="height: 300px">
+			<scroll-view class="right-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
 				<view>yyy</view>
 				<view>yyy</view>
 				<view>yyy</view>
@@ -47,12 +79,28 @@
 	export default {
 		data() {
 			return {
-				
+				//当前设备可用的高度
+				wh: 0
 			};
+		},
+		
+		onLoad() {
+			// 获取当前系统的信息
+			const sysInfo = uni.getSystemInfoSync()
+			// 为 wh 窗口可用高度动态赋值
+			this.wh = sysInfo.windowHeight
 		}
 	}
+	
+	
 </script>
 
 <style lang="scss">
+.scroll-view-container{
+	display: flex;
+}
+.left-scroll-view{
+	width: 120 px;
+}
 
 </style>
