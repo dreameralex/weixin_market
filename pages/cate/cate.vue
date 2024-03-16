3ec61e454a592157343a63d661794b5481763837
Alex <zhangyh8216@sina.com>
2024-03-16 20:10:47 +0800

    滑动效果1

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,45 @@
 <template>
 	<view>
-		cate
+		<view class="scroll-view-container">
+			<!-- 左侧的滑动区-->
+			<scroll-view scroll-y="true" style="height: 300px">
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+				<view>xxx</view>
+			</scroll-view>
+			<!-- 右侧的滑动区-->
+			<scroll-view scroll-y="true" style="height: 300px">
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+				<view>yyy</view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
